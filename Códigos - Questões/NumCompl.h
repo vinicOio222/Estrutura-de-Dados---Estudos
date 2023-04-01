e8dd1690f0838966b449b8264d9deb6a80cd60e6
Vinícius dos Santos Alves <viniciuus.santos@aluno.uece.br>
2023-04-01 01:34:59 -0300

    Update NumCompl.h

--- Códigos - Questões/NumCompl.h
+++ Códigos - Questões/NumCompl.h
@@ -4,3 +4,5 @@ Complexo* criar_complexo(float x, float y);
 void destruir_complexo(Complexo* c);
 void somar_complexos(Complexo *c1, Complexo *c2);
 void multiplicar_complexos(Complexo *c1, Complexo *c2);
+
+
