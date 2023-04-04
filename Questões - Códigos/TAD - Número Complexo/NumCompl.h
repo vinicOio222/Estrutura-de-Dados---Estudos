21aacd3f1263f9a88de465146f5ce14879275b5c
Vinícius dos Santos Alves <viniciuus.santos@aluno.uece.br>
2023-04-03 21:35:13 -0300

    Create NumCompl.h

---
+++ Questões - Códigos/TAD - Número Complexo/NumCompl.h
@@ -0,0 +1,7 @@
+typedef struct complexo Complexo;
+
+Complexo* criar_complexo(float x, float y);
+void destruir_complexo(Complexo* c);
+void somar_complexos(Complexo *c1, Complexo *c2);
+void multiplicar_complexos(Complexo *c1, Complexo *c2);
+
