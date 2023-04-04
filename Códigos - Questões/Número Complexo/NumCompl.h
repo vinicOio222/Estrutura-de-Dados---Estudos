834aa0ad748cb220d1529545b4966fb49dc1f68e
Vinícius dos Santos Alves <viniciuus.santos@aluno.uece.br>
2023-04-03 21:30:21 -0300

    Delete Códigos - Questões/Número Complexo directory

--- Códigos - Questões/Número Complexo/NumCompl.h
+++
@@ -1,6 +0,0 @@
-typedef struct complexo Complexo;
-
-Complexo* criar_complexo(float x, float y);
-void destruir_complexo(Complexo* c);
-void somar_complexos(Complexo *c1, Complexo *c2);
-void multiplicar_complexos(Complexo *c1, Complexo *c2);
