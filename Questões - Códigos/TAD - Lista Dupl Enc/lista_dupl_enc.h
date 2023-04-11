d190edaef66eba21ef7b11eac3683a0733e2dc76
Vinícius dos Santos Alves <viniciuus.santos@aluno.uece.br>
2023-04-11 20:26:17 -0300

    Add files via upload

---
+++ Questões - Códigos/TAD - Lista Dupl Enc/lista_dupl_enc.h
@@ -0,0 +1,15 @@
+struct nodo{
+    int dados;
+    struct nodo* ant;
+    struct nodo* prox;
+
+};
+
+typedef struct nodo Nodo;
+typedef struct lista_enc ListaEnc;
+
+ListaEnc* cria_lista_dupl();
+void inserir_elemento_inicio(ListaEnc *li, int valor);
+void remover_elemento_final(ListaEnc* li);
+void imprimir_lista(ListaEnc* li);
+
