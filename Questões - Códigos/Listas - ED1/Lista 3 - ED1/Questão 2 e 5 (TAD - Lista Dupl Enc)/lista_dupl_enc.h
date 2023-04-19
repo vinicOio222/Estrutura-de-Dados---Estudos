6f456f4b276ee229013b9f812649be0a43fd4959
Vinícius dos Santos Alves <viniciuus.santos@aluno.uece.br>
2023-04-18 21:03:15 -0300

    Update lista_dupl_enc.h

--- Questões - Códigos/Listas - ED1/Lista 3 - ED1/Questão 2 e 5 (TAD - Lista Dupl Enc)/lista_dupl_enc.h
+++ Questões - Códigos/Listas - ED1/Lista 3 - ED1/Questão 2 e 5 (TAD - Lista Dupl Enc)/lista_dupl_enc.h
@@ -9,7 +9,7 @@ typedef struct nodo Nodo;
 typedef struct lista_enc ListaEnc;
 
 ListaEnc* cria_lista_dupl();
-void inserir_elemento_inicio(ListaEnc *li, int valor);
+void inserir_elemento_final(ListaEnc *li, int valor);
 void remover_elemento_inicio(ListaEnc* li);
 void imprimir_lista(ListaEnc* li);
 
